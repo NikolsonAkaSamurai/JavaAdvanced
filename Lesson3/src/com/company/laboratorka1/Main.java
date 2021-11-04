4879d55d4fc3b53d8349133010c95917e18e77de
Nikolai Syuris <nik.syu34@gmail.com>
2021-11-04 21:36:11 +0300

    laboratorla1 solved

--- Lesson3/src/com/company/laboratorka1/Main.java
+++ Lesson3/src/com/company/laboratorka1/Main.java
@@ -1,7 +1,23 @@
 package com.company.laboratorka1;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Field;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("впишите название файла (file)");
+        String begin = "src/com/company/laboratorka1/";
+        String path = begin += scn.next();
+
+        File file = new File(path);
+        Scanner readFile = new Scanner(file);
+
+        while (readFile.hasNext()){
+            System.out.println(readFile.next());
+        }
+        System.out.println("файл прочитан!");
     }
 }
