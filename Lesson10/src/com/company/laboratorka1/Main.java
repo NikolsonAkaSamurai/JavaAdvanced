59c9a5826e8e7f5b635cbf7207b0bc850975c094
Nikolai Syuris <nik.syu34@gmail.com>
2021-11-23 21:56:21 +0300

    лабораторка 1.2

--- Lesson10/src/com/company/laboratorka1/Main.java
+++ Lesson10/src/com/company/laboratorka1/Main.java
@@ -14,8 +14,25 @@ public class Main {
 
         MyInterface myInterface = (a) -> a % 2 == 0;
         for (Integer num: nums){
-            System.out.println(myInterface.isEven(num));
+            System.out.println("число " + num + " четное? " + myInterface.myMethod(num));
+        }
 
+        System.out.println("================");
+        MyInterface myInterface1 = a -> a % 2.0 != 0;
+        for (Integer num: nums){
+            System.out.println("число " + num + " нечетное? " + myInterface1.myMethod(num));
+        }
+
+        System.out.println("================");
+        MyInterface myInterface2 = a -> a >= 3 && a < 14;
+        for(Integer num: nums){
+            System.out.println("число " + num + " входит  диапазон? " + myInterface2.myMethod(num));
+        }
+
+        System.out.println("================");
+        MyInterface myInterface3 = a -> a % 3 == 0;
+        for(Integer num: nums){
+            System.out.println("число " + num + " кратно 3? "  + myInterface3.myMethod(num));
         }
 
     }
@@ -25,5 +42,5 @@ public class Main {
 }
 @FunctionalInterface
 interface MyInterface{
-    boolean isEven(int a);
+    boolean myMethod(int a);
 }
